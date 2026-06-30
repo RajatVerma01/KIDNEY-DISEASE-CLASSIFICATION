34b585ed1252e909243648db13b9f37f4ca83bb5
RajatVerma01 <rajatverma11803@gmail.com>
2026-06-30 16:32:09 +0530

    requirements and logger added

---
+++ setup.py
@@ -0,0 +1,9 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name="kidney_disease_classification",
+    version="0.0.0",
+    packages=find_packages(),
+    author="Rajat Verma"
+)
---
+++ src/cnnClassifier/__init__.py
@@ -0,0 +1,1 @@
+from src.cnnClassifier.logger import logger
